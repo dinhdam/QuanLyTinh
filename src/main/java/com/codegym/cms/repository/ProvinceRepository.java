e93f9b4c068d663670a6ec42d28537a27d2e2d4d
= <=>
2018-10-31 16:51:40 +0700

    quan ly khach hang

---
+++ src/main/java/com/codegym/cms/repository/ProvinceRepository.java
@@ -0,0 +1,5 @@
+package com.codegym.cms.repository;
+import com.codegym.cms.model.Province;
+import org.springframework.data.repository.PagingAndSortingRepository;
+public interface ProvinceRepository extends PagingAndSortingRepository<Province, Long> {
+}
